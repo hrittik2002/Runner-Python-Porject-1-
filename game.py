b99ee47f4da4ffc4757f857eddb4fdafc6b34cf0
Hrittik <hrittikgarain@gmail.com>
2021-10-21 17:48:29 +0530

    checkpoint 4

--- game.py
+++ game.py
@@ -1,6 +1,86 @@
 import pygame
 from sys import exit
-from random import randint
+from random import randint , choice
+
+
+class Player(pygame.sprite.Sprite):  # Inherit from Spritre class
+
+    def __init__(self):  # Constractor
+        super().__init__() # we are initializing the sprite class right inside the Player class so that we can access it
+        player_walk_1 = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()  # image for player
+        player_walk_2 = pygame.image.load('graphics/Player/player_walk_2.png').convert_alpha()  # image for player
+        self.player_walk = [player_walk_1 , player_walk_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()  # image for player jump
+        # Now this class needed 2 attribute at the very minimum one is self.image and the other self.rect
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom=(80, 300))
+        self.gravity = 0
+
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(0.3)
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+            self.gravity = -20
+            self.jump_sound.play()
+    
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300
+    
+    def animation_state(self):
+        if(self.rect.bottom < 300):
+            #jump
+            self.image = self.player_jump
+        else:
+            #walk
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk): self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self , type):
+        super().__init__()
+
+        if type == 'fly':
+            fly_1 = pygame.image.load('graphics/Fly/Fly1.png').convert_alpha()
+            fly_2 = pygame.image.load('graphics/Fly/Fly2.png').convert_alpha()
+            self.frames = [fly_1 , fly_2]
+            y_pos = 210
+        else:
+            snail_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()  # image for snail 1
+            snail_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()  # image for snail 2
+            self.frames = [snail_1 , snail_2]
+            y_pos = 300
+        
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (randint(900 , 1100) , y_pos))
+    
+    def animation_state(self):
+        self.animation_index += 0.1
+        if self.animation_index >= len(self.frames):
+            self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+    def  update(self):
+        self.animation_state()
+        self.rect.x -= 6
+        self.destroy()
+    
+    def destroy(self):
+        if self.rect.x <= -100:
+            self.kill()
 
 # for displaying the score
 def display_score():
@@ -31,7 +111,14 @@ def collisions(player , obstacles):
                 return False
     return True
 
+def collisions_sprite():
+    if pygame.sprite.spritecollide(player.sprite , obstacle_group, False):
+        obstacle_group.empty()
+        return False
+    else:
+        return True
 def player_animation():
+
     global player_surf , player_index
 
     if(player_rect.bottom < 300):
@@ -42,6 +129,9 @@ def player_animation():
         player_index += 0.1
         if player_index >= len(player_walk): player_index = 0
         player_surf = player_walk[int(player_index)]
+
+
+
 # here we are initializing pygame (Note it should always be the first line)
 pygame.init()
 screen = pygame.display.set_mode((800, 400))    # to create the display surface
@@ -52,6 +142,17 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 game_active = False
+
+bg_Music = pygame.mixer.Sound('audio/music.wav')
+bg_Music.set_volume(0.1)
+bg_Music.play(loops = -1)
+
+
+# Groups
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+obstacle_group = pygame.sprite.Group()
 
 # background image for sky
 sky_surface = pygame.image.load('graphics/MySky.png').convert()
@@ -139,10 +240,11 @@ while True:                            # while this condition is true the screen
 
         if game_active:
             if event.type == obstacle_timer:
-                if randint(0 , 2): # eturn 0 or 1
-                    obstacle_rect_list.append(snail_surface.get_rect(bottomright=(randint(900 , 1100), 300)))       
-                else:
-                    obstacle_rect_list.append(fly_surf.get_rect(bottomright=(randint(900 , 1100), 210))) 
+                obstacle_group.add(Obstacle(choice(['fly' , 'snail' , 'snail' , 'snail'])))
+                # if randint(0 , 2): # eturn 0 or 1
+                #     obstacle_rect_list.append(snail_surface.get_rect(bottomright=(randint(900 , 1100), 300)))       
+                # else:
+                #     obstacle_rect_list.append(fly_surf.get_rect(bottomright=(randint(900 , 1100), 210))) 
             
             if event.type == snail_animation_timer:
                 if snail_frame_index == 0: snail_frame_index = 1
@@ -170,18 +272,26 @@ while True:                            # while this condition is true the screen
 
 
         # PLAYERS JUMP LOGIC
-        player_gravity += 1
-        player_rect.y += player_gravity
+        # player_gravity += 1
+        # player_rect.y += player_gravity
 
-        if player_rect.bottom >= 300:
-            player_rect.bottom = 300
-        player_animation()
-        screen.blit(player_surf , player_rect)
+        # if player_rect.bottom >= 300:
+        #     player_rect.bottom = 300
+        # player_animation()
+        # screen.blit(player_surf , player_rect)
+
+        player.draw(screen)
+
+        player.update()
         
+        obstacle_group.draw(screen)
+        obstacle_group.update()
+
         # OBSTACLE MOVEMENT
-        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+        # obstacle_rect_list = obstacle_movement(obstacle_rect_list)
         # COLLISION BETWEEN PLAYER AND OBSTACLE
-        game_active = collisions(player_rect , obstacle_rect_list)
+        game_active = collisions_sprite()
+        # game_active = collisions(player_rect , obstacle_rect_list)
         
     else:    
         screen.fill((94,129,162))
